592f127f5aaca9aee33ed7b26119c2e231c3399b
Bhuvan Prasad Dahal <bhuvandahal6@gmail.com>
2024-03-23 19:58:37 +0545

    Modify companies page UI

--- components/page/Companies.tsx
+++ components/page/Companies.tsx
@@ -1,6 +1,13 @@
 const Companies = () => {
     return (
-        <div>Companies</div>
+        <section className="flex-1 min-h-screen p-4 flex flex-col items-center justify-center gap-3">
+            <img
+                src="/images/buildings.png"
+                alt="companies"
+                className="h-[100px]"
+            />
+            <p className="text-muted-foreground">We don't have any companies.</p>
+        </section>
     )
 };
 
