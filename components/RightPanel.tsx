d6e18ce4584e787fe07c33aa57ff7263f0a7b794
Bhuvan Prasad Dahal <bhuvandahal6@gmail.com>
2024-03-27 14:08:23 +0545

    Modify right panel UI

--- components/RightPanel.tsx
+++ components/RightPanel.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { GoIssueOpened } from "react-icons/go";
+import { LuHeartHandshake } from "react-icons/lu";
 
 const RightPanel = () => {
     return (
@@ -7,19 +9,25 @@ const RightPanel = () => {
                 <li className="text-zinc-900">
                     <h4 className="text-[13px] font-semibold px-3 py-2 border-b border-amber-500">CONTRIBUTE</h4>
                     <ul className="text-sm px-3 py-2 space-y-1">
-                        <li>
-                            • Want to contribute to this project? Go to our
-                            <Link
-                                href="https://github.com/BhuvanPdDahal/CodeOverflow"
-                                className='text-blue-700 ml-1 hover:text-blue-800'
-                            >github repo</Link>.
+                        <li className="flex gap-2">
+                            <LuHeartHandshake className="h-6 w-6 text-zinc-800" />
+                            <p>
+                                Want to contribute to this project? Go to our
+                                <Link
+                                    href="https://github.com/BhuvanPdDahal/CodeOverflow"
+                                    className='text-blue-700 ml-1 hover:text-blue-800'
+                                >github repo</Link>.
+                            </p>
                         </li>
-                        <li>
-                            • Have an issue with the project? Create an issue
-                            <Link
-                                href="https://github.com/BhuvanPdDahal/CodeOverflow/issues/new"
-                                className='text-blue-700 ml-1 hover:text-blue-800'
-                            >here</Link>.
+                        <li className="flex gap-2">
+                            <GoIssueOpened className="h-5 w-5 text-zinc-800" />
+                            <p>
+                                Have an issue with the project? Create an issue
+                                <Link
+                                    href="https://github.com/BhuvanPdDahal/CodeOverflow/issues/new"
+                                    className='text-blue-700 ml-1 hover:text-blue-800'
+                                >here</Link>.
+                            </p>
                         </li>
                     </ul>
                 </li>
--- components/page/Companies.tsx
+++ components/page/Companies.tsx
@@ -1,12 +1,12 @@
 const Companies = () => {
     return (
-        <section className="flex-1 min-h-screen p-4 flex flex-col items-center justify-center gap-3">
+        <section className="flex-1 min-h-rem p-4 flex flex-col items-center justify-center gap-3">
             <img
                 src="/images/buildings.png"
                 alt="companies"
                 className="h-[100px]"
             />
-            <p className="text-muted-foreground">We don't have any companies.</p>
+            <p className="text-[15px] text-muted-foreground">We don't have any companies.</p>
         </section>
     )
 };
